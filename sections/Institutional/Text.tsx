438bb33a5031a0280fdca5b52b536095d0dd178c
decobot <capy@deco.cx>
2024-11-13 16:36:08 -0300

    .

--- sections/Institutional/Text.tsx
+++ sections/Institutional/Text.tsx
@@ -9,7 +9,7 @@ const Text = ({ html }: Props) => {
     <>
       {html && (
         <div
-          class="fluid-text text-[12px] lg:text-[14px] mb-8 container px-5"
+          class="fluid-text text-xs lg:text-sm mb-8 container px-5"
           dangerouslySetInnerHTML={{ __html: html }}
         />
       )}
